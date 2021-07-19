dbfcc81f678a5cfbeb4778a2fd32c5b78885c993
Kirill <kupuk132@gmail.com>
2021-07-20 00:26:39 +0300

    create ClusterServer

---
+++ ClusterServer/cluster.js
@@ -0,0 +1,28 @@
+const cluster = require('cluster');
+const os = require('os');
+const pid = process.pid;
+
+if(cluster.isMaster) {
+  const cpusCount = os.cpus().length;
+  console.log(`CPUs: ${cpusCount}`);
+  console.log(`Master started. Pid: ${pid}`);
+  for(let i = 0; i < cpusCount-1; i++ ) {
+    const worker = cluster.fork();
+    worker.on('exit', () => {
+      console.log(`Worker died! Pid: ${worker.process.pid}`);
+      cluster.fork();
+    });
+    worker.send('Hello from server!');
+    worker.on('message', (msg) => {
+      console.log(`Message from worker: ${worker.process.pid} : ${JSON.stringify(msg)}`)
+    })
+  }
+}
+
+if(cluster.isWorker) {
+  require('./worker.js');
+  process.on('message', (msg) => {
+    console.log(`Message from master: ${msg}`);
+  });
+  process.send({text: 'Hello', pid})
+}
